a7dc4f349f58c65ccc2641ce49dc38a875a5ed04
zhuyx <zhuyx@startimes.com.cn>
2020-09-30 17:07:04 +0800

    nacos docker-compose file

---
+++ deploy/README.md
@@ -0,0 +1,7 @@
+clone:
+```
+git clone https://github.com/nacos-group/nacos-docker.git
+cd nacos-docker
+
+docker-compose -f example/standalone-mysql.yaml up
+```
--- service/customer/customer-service/src/main/java/com/star/customer/CustomerApplication.java
+++ service/customer/customer-service/src/main/java/com/star/customer/CustomerApplication.java
@@ -5,9 +5,14 @@ import com.star.common.jersey.RpcHeaderFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @EnableFeignClients(basePackages = {
         "com.star.partner.api.business"
@@ -26,5 +31,15 @@ public class CustomerApplication {
         rs.register(RpcHeaderFilter.class);
         rs.register(ExMapper.class);
         return rs;
+    }
+
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMinutes(1))
+                .setReadTimeout(Duration.ofMinutes(1))
+                .build();
     }
 }
